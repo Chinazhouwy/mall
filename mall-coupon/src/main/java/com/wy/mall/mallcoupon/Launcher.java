346f98eabb9bf8051425d4421886f988f38117ec
wy <chinazhouwy@gmail.com>
2025-12-16 22:45:33 +0800

    feat(project): renrenfast引入和项目编译调整

--- mall-coupon/src/main/java/com/wy/mall/mallcoupon/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.wy.mall.mallcoupon;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- mall-order/src/main/java/com/wy/mall/mallorder/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.wy.mall.mallorder;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
